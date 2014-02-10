dcb9402ad9e4619d3286108b70466fdb85d47f7e
Крыцкий Андрей <kav.it@mail.ru>
2014-02-11 01:03:50 +0300

    Добавлен MSBuild для создания Zip

--- Build/Utils.bat
+++ Build/Utils.bat
@@ -1,6 +1,8 @@
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe "Rebuild.xml" /t:DeleteFiles
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe "Utils.xml" /t:DeleteFiles
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe "Rebuild.xml" /t:IncVersion /p:Version1=145 /p:Version2=42 /p:Version3=33 /p:Version4=1124
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe "Utils.xml" /t:IncVersion /p:Version=1.2.3.4
+
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe "Utils.xml" /t:Deploy /p:DeployPath="e:\Disk D\_build\TeamCity.Result\SharpLib\1.2.3.4" /p:Version=1.3.3.3
 
 pause
 
