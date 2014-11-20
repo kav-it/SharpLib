3dd76dfab94e1b43f56b914530cc3129aac61dc1
Крыцкий Андрей <kav.it@mail.ru>
2014-11-21 01:47:30 +0400

    Добавлен GroupBox

---
+++ Source/SharpLib.Wpf/Source/Controls/Classes/GroupBoxEx.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SharpLib.Wpf.Controls
+{
+    public class GroupBoxEx : GroupBox
+    {
+        #region Конструктор
+
+        static GroupBoxEx()
+        {
+            // Переопределение стиля по умолчанию
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(GroupBoxEx), new FrameworkPropertyMetadata(typeof(GroupBoxEx)));
+        }
+
+        #endregion
+    }
+}
---
+++ Source/SharpLib.Wpf/Source/Controls/Classes/RadioButtonEx.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SharpLib.Wpf.Controls
+{
+    public class RadioButtonEx : RadioButton
+    {
+        #region Конструктор
+
+        static RadioButtonEx()
+        {
+            // Переопределение стиля по умолчанию
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(RadioButtonEx), new FrameworkPropertyMetadata(typeof(RadioButtonEx)));
+
+            // Установка вертикального выравнивания текста по умолчанию
+            VerticalContentAlignmentProperty.OverrideMetadata(typeof(RadioButtonEx), new FrameworkPropertyMetadata(VerticalAlignment.Center));
+        }
+
+        #endregion
+    }
+}
