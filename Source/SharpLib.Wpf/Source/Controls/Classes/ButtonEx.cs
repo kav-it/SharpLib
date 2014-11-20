74f592c19fbc9a54c7d7b2d31d5b45b693f6cb41
Крыцкий Андрей <kav.it@mail.ru>
2014-11-21 01:06:42 +0400

    Добавлен CheckBoxEx

---
+++ Source/SharpLib.Wpf/Source/Controls/Classes/ButtonEx.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SharpLib.Wpf.Controls
+{
+    public class ButtonEx : Button
+    {
+        #region Конструктор
+
+        static ButtonEx()
+        {
+            // Переопределение стиля по умолчанию
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ButtonEx), new FrameworkPropertyMetadata(typeof(ButtonEx)));
+
+            WidthProperty.OverrideMetadata(typeof(ButtonEx), new FrameworkPropertyMetadata((double)80));
+        }
+
+        #endregion
+    }
+}
---
+++ Source/SharpLib.Wpf/Source/Controls/Classes/CheckBoxEx.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SharpLib.Wpf.Controls
+{
+    public class CheckBoxEx : CheckBox
+    {
+        #region Конструктор
+
+        static CheckBoxEx()
+        {
+            // Переопределение стиля по умолчанию
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CheckBoxEx), new FrameworkPropertyMetadata(typeof(CheckBoxEx)));
+
+            // Установка вертикального выравнивания текста по умолчанию
+            VerticalContentAlignmentProperty.OverrideMetadata(typeof(CheckBoxEx), new FrameworkPropertyMetadata(VerticalAlignment.Center));
+        }
+
+        #endregion
+    }
+}
