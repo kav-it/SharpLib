dc0701b777140d749a6c3c0d5e6482bab176dd61
Крыцкий Андрей <kav.it@mail.ru>
2014-11-26 01:51:18 +0400

    Добавлен Extensions для Assembly

---
+++ Source/SharpLib/Source/Extensions/Extension.Assembly.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace SharpLib
+{
+    public static class ExtensionAssembly
+    {
+        #region Методы
+
+        /// <summary>
+        /// Признак сборки в отладочной конфигурации
+        /// </summary>
+        /// <param name="assembly"></param>
+        /// <returns></returns>
+        public static bool IsDebugEx(this Assembly assembly)
+        {
+            var result = assembly
+                .GetCustomAttributes(false)
+                .OfType<DebuggableAttribute>()
+                .Select(debuggableAttribute => debuggableAttribute.IsJITTrackingEnabled)
+                .FirstOrDefault();
+
+            return result;
+        }
+
+        /// <summary>
+        /// Дата/время создания сборки
+        /// </summary>
+        public static DateTime GetStampEx(this Assembly assembly)
+        {
+            var fileInfo = new FileInfo(assembly.Location);
+            var result = fileInfo.LastWriteTime;
+
+            return result;
+        }
+
+        /// <summary>
+        /// Версия сборки
+        /// </summary>
+        public static Version GetVersionEx(this Assembly assembly)
+        {
+            var attr = assembly.GetAssemblyAttributeEx<AssemblyFileVersionAttribute>();
+
+            return new Version(attr.Version);
+        }
+
+        /// <summary>
+        /// Название сборки
+        /// </summary>
+        public static string GetTitleEx(this Assembly assembly)
+        {
+            var attr = assembly.GetAssemblyAttributeEx<AssemblyTitleAttribute>();
+
+            return attr.Title;
+        }
+
+
+        /// <summary>
+        /// Чтение атрибута из сборки
+        /// </summary>
+        public static T GetAssemblyAttributeEx<T>(this Assembly assembly) where T : Attribute
+        {
+            var attributes = assembly.GetCustomAttributes(typeof(T), false);
+            if (attributes.Length == 0)
+            {
+                return null;
+            }
+            return attributes.OfType<T>().SingleOrDefault();
+        }
+
+        #endregion
+    }
+}
--- Source/SharpLib/Source/Helpers/Bootstrap/Application.cs
+++ Source/SharpLib/Source/Helpers/Bootstrap/Application.cs
@@ -1,16 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace SharpLib
 {
     public class SharpLibApp
     {
-
         /// <summary>
         /// Время запуска приложения
         /// </summary>
@@ -32,26 +26,46 @@ namespace SharpLib
         /// <summary>
         /// Версия приложения
         /// </summary>
-        public static ModuleVersion Version { get; private set; }
+        public static Version Version { get; private set; }
 
         /// <summary>
         /// Текстовое название приложения
         /// </summary>
-        public static String Title
-        {
-            get { return Version.Title; }
-        }
+        public static string Title { get; private set; }
 
         /// <summary>
         /// Данные о расположении приложения (директория, имя, расширение)
         /// </summary>
         public static FileLocation Location { get; private set; }
 
+        /// <summary>
+        /// Признак отладочной конфигурации
+        /// </summary>
+        public static bool IsDebug { get; private set; }
+
+        /// <summary>
+        /// Полное название с версией и признаком отладки
+        /// </summary>
+        /// <example>
+        /// Монитор ServiceD (2.0.1.12) [debug]
+        /// </example>
+        public static string TitleFull { get; private set; }
+        
+        /// <summary>
+        /// Инициализация (должна вызываться в самом начале при старте приложения)
+        /// </summary>
         public static void Init()
         {
-            Version = ModuleVersion.UpdateFromAssemblyInstance(Assembly.GetEntryAssembly());
+            var asm = Assembly.GetEntryAssembly();
+
+            Version = asm.GetVersionEx();
+            Title = asm.GetTitleEx();
+            IsDebug = asm.IsDebugEx();
             StartTime = DateTime.Now;
-            Thread.CurrentThread.Name = "Application";
+            Location = new FileLocation(asm.Location);
+
+            var debug = IsDebug ? "[debug]" : string.Empty;
+            TitleFull = string.Format("{0} ({1}) {2}", Title, Version, debug);
         }
     }
 }
