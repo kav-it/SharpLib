91cd8207d98d62fe3e1ae1f16a4fa254a8397727
Крыцкий Андрей <kav.it@mail.ru>
2015-01-30 18:24:42 +0400

    Исправлена ошибка сборки

--- Build/build.bat
+++ Build/build.bat
@@ -4,23 +4,26 @@
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe "..\Source\SharpLib.WinForms\SharpLib.WinForms.csproj" /t:rebuild /p:Configuration=Debug
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe "..\Source\SharpLib.Wpf\SharpLib.Wpf.csproj" /t:rebuild /p:Configuration=Debug
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe "..\Source\SharpLib.OpenGL\SharpLib.OpenGL.csproj" /t:rebuild /p:Configuration=Debug
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe "..\Source\SharpLib.Audio\SharpLib.Audio.csproj" /t:rebuild /p:Configuration=Debug
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe "..\Source\SharpLib.Json\SharpLib.Json.csproj" /t:rebuild /p:Configuration=Debug
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe "..\Source\SharpLib.Log\SharpLib.Log.csproj" /t:rebuild /p:Configuration=Debug
 
-xcopy /Y "..\Source\SharpLib\bin\Debug\SharpLib.dll" "..\BuildResult\"
-xcopy /Y "..\Source\SharpLib\bin\Debug\SharpLib.pdb" "..\BuildResult\"
-xcopy /Y "..\Source\SharpLib.Dialogs\bin\Debug\SharpLib.Dialogs.dll" "..\BuildResult\"
-xcopy /Y "..\Source\SharpLib.Dialogs\bin\Debug\SharpLib.Dialogs.pdb" "..\BuildResult\"
-xcopy /Y "..\Source\SharpLib.Native\bin\Debug\SharpLib.Native.dll" "..\BuildResult\"
-xcopy /Y "..\Source\SharpLib.Native\bin\Debug\SharpLib.Native.pdb" "..\BuildResult\"
-xcopy /Y "..\Source\SharpLib.WinForms\bin\Debug\SharpLib.WinForms.dll" "..\BuildResult\"
-xcopy /Y "..\Source\SharpLib.WinForms\bin\Debug\SharpLib.WinForms.pdb" "..\BuildResult\"
-xcopy /Y "..\Source\SharpLib.Wpf\bin\Debug\SharpLib.Wpf.dll" "..\BuildResult\"
-xcopy /Y "..\Source\SharpLib.Wpf\bin\Debug\SharpLib.Wpf.pdb" "..\BuildResult\"
-xcopy /Y "..\Source\SharpLib.OpenGL\bin\Debug\SharpLib.OpenGL.dll" "..\BuildResult\"
-xcopy /Y "..\Source\SharpLib.OpenGL\bin\Debug\SharpLib.OpenGL.pdb" "..\BuildResult\"
-xcopy /Y "..\Source\SharpLib.Log\bin\Debug\SharpLib.Log.dll" "..\BuildResult\"
-xcopy /Y "..\Source\SharpLib.Log\bin\Debug\SharpLib.Log.pdb" "..\BuildResult\"
-xcopy /Y "..\Source\SharpLib.Json\bin\Debug\SharpLib.Json.dll" "..\BuildResult\"
-xcopy /Y "..\Source\SharpLib.Json\bin\Debug\SharpLib.Json.pdb" "..\BuildResult\"
+copy /Y "..\Source\SharpLib\bin\Debug\SharpLib.dll" "..\BuildResult\"
+copy /Y "..\Source\SharpLib\bin\Debug\SharpLib.pdb" "..\BuildResult\"
+copy /Y "..\Source\SharpLib.Audio\bin\Debug\SharpLib.Audio.dll" "..\BuildResult\"
+copy /Y "..\Source\SharpLib.Audio\bin\Debug\SharpLib.Audio.pdb" "..\BuildResult\"
+copy /Y "..\Source\SharpLib.Native\bin\Debug\SharpLib.Native.dll" "..\BuildResult\"
+copy /Y "..\Source\SharpLib.Native\bin\Debug\SharpLib.Native.pdb" "..\BuildResult\"
+copy /Y "..\Source\SharpLib.WinForms\bin\Debug\SharpLib.WinForms.dll" "..\BuildResult\"
+copy /Y "..\Source\SharpLib.WinForms\bin\Debug\SharpLib.WinForms.pdb" "..\BuildResult\"
+copy /Y "..\Source\SharpLib.Wpf\bin\Debug\SharpLib.Wpf.dll" "..\BuildResult\"
+copy /Y "..\Source\SharpLib.Wpf\bin\Debug\SharpLib.Wpf.pdb" "..\BuildResult\"
+copy /Y "..\Source\SharpLib.OpenGL\bin\Debug\SharpLib.OpenGL.dll" "..\BuildResult\"
+copy /Y "..\Source\SharpLib.OpenGL\bin\Debug\SharpLib.OpenGL.pdb" "..\BuildResult\"
+copy /Y "..\Source\SharpLib.Log\bin\Debug\SharpLib.Log.dll" "..\BuildResult\"
+copy /Y "..\Source\SharpLib.Log\bin\Debug\SharpLib.Log.pdb" "..\BuildResult\"
+copy /Y "..\Source\SharpLib.Json\bin\Debug\SharpLib.Json.dll" "..\BuildResult\"
+copy /Y "..\Source\SharpLib.Json\bin\Debug\SharpLib.Json.pdb" "..\BuildResult\"
 
 pause
 
