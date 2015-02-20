74ca777470599a50508cfe8a21685b84245791c0
Крыцкий Андрей <kav.it@mail.ru>
2015-02-21 01:07:20 +0400

    Импорт проекта Docking

---
+++ Source/SharpLib.Docking/Source/Version/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+using System.Windows;
+
+[assembly: AssemblyTitle("Плавающие панели")]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
