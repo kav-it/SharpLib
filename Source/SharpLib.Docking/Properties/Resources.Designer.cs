992008e1d0a7b15fb7f15b69bc1a3f590816cba8
Крыцкий Андрей <kav.it@mail.ru>
2015-02-21 02:23:16 +0400

    Исправлена ошибка запуска

--- Source/SharpLib.Docking/Properties/Resources.Designer.cs
+++ Source/SharpLib.Docking/Properties/Resources.Designer.cs
@@ -55,7 +55,7 @@ namespace SharpLib.Docking.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Xceed.Wpf.AvalonDock.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SharpLib.Docking.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
