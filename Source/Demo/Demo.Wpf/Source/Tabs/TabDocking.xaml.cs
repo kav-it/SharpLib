ef761a9d64c0dfc92628919bd1b2a3fc6cbab9a6
Крыцкий Андрей <kav.it@mail.ru>
2015-02-21 01:55:38 +0400

    Добавлен тестовый UI

--- Source/Demo/Demo.Wpf/Source/Tabs/TabDocking.xaml.cs
+++ Source/Demo/Demo.Wpf/Source/Tabs/TabDocking.xaml.cs
@@ -1,19 +1,89 @@
 ﻿using System;
-using System.Reflection;
 using System.Windows;
-using System.Windows.Controls.Primitives;
+using System.Windows.Controls;
 
-using SharpLib;
-using SharpLib.Audio;
-using SharpLib.Audio.Wave;
+using Xceed.Wpf.AvalonDock.Layout;
 
 namespace DemoWpf
 {
     public partial class TabDocking
     {
-        private void AddLeftButtonClick(object sender, RoutedEventArgs e)
+        #region Конструктор
+
+        /// <summary>
+        /// Левая панель
+        /// </summary>
+        internal LayoutAnchorablePane LeftPanel { get; private set; }
+
+        /// <summary>
+        /// Правая панель
+        /// </summary>
+        private LayoutAnchorablePane _rightPanel;
+
+        /// <summary>
+        /// Центральная панель
+        /// </summary>
+        internal LayoutDocumentPane CenterPanel { get; private set; }
+
+        /// <summary>
+        /// Нижняя панель
+        /// </summary>
+        private LayoutAnchorGroup _bottomPanel;
+
+        public TabDocking()
         {
-            // PART_dockingManager
+            InitializeComponent();
+
+            this.Loaded += OnLoaded;
+
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            CreateLayout();
+        }
+
+        #endregion
+
+        #region Методы
+
+        /// <summary>
+        /// Создание Layout
+        /// </summary>
+        private void CreateLayout()
+        {
+            // Горизонтальная панель <Левый контент> + <Центр> + <Правый контент>
+            var horizontalPanel = new LayoutPanel { Orientation = Orientation.Horizontal };
+
+            LeftPanel = new LayoutAnchorablePane { DockWidth = new GridLength(300) };
+            CenterPanel = new LayoutDocumentPane();
+            _rightPanel = new LayoutAnchorablePane { DockWidth = new GridLength(300) };
+            _bottomPanel = new LayoutAnchorGroup();
+
+            horizontalPanel.Children.Add(LeftPanel);
+            horizontalPanel.Children.Add(CenterPanel);
+            horizontalPanel.Children.Add(_rightPanel);
+
+            PART_rootLayout.RootPanel = horizontalPanel;
+            PART_rootLayout.BottomSide.Children.Add(_bottomPanel);
+        }
+
+        private void AddCenterClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        #endregion
+
+        private void AddLeftClick(object sender, RoutedEventArgs e)
+        {
+            var container = new LayoutAnchorable
+            {
+                Content = new Button {Content = "Left 1"},
+                Title = "Title",
+                ContentId = Guid.NewGuid().ToString()
+            };
+
+            LeftPanel.Children.Add(container);
         }
     }
 }
