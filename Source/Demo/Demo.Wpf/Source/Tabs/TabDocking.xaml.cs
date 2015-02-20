3e26b84f7ef20d7f60dc9c8ce87fc557152786d3
Крыцкий Андрей <kav.it@mail.ru>
2015-02-21 01:21:43 +0400

    Удалена верси 1.3

---
+++ Source/Demo/Demo.Wpf/Source/Tabs/TabDocking.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+using SharpLib;
+using SharpLib.Audio;
+using SharpLib.Audio.Wave;
+
+namespace DemoWpf
+{
+    public partial class TabDocking
+    {
+        private void AddLeftButtonClick(object sender, RoutedEventArgs e)
+        {
+            // PART_dockingManager
+        }
+    }
+}
