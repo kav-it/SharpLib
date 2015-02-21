2b262d0add31a34a4e394f0b15875c8f9189e59a
Крыцкий Андрей <kav.it@mail.ru>
2015-02-21 23:44:16 +0400

    Рефакторинг 13 Добавлен Метро

---
+++ Source/SharpLib.Docking/Source/Themes/Theme.Metro.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SharpLib.Docking.Themes
+{
+    public class MetroTheme : Theme
+    {
+        public override Uri GetResourceUri()
+        {
+            return new Uri("/SharpLib.Docking;component/Themes/metro.xaml", UriKind.Relative);
+        }
+    }
+}
