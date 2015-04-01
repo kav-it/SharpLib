6c3e7fe3e45d2fcaa3c646ab153ae4c1e70496a3
Крыцкий Андрей <kav.it@mail.ru>
2015-04-02 00:54:35 +0300

    Работает отображение TextEditor

--- Source/SharpLib.TextEditor/Source/Version/AssemblyInfo.cs
+++ Source/SharpLib.TextEditor/Source/Version/AssemblyInfo.cs
@@ -1,3 +1,6 @@
 ﻿using System.Reflection;
+using System.Windows;
 
 [assembly: AssemblyTitle("Текстовый редактор")]
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
